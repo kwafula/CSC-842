174eb9f0bc40be8f4b044a0039fdb0ecd232bed3
Kevin Wafula <95890992+kwafula@users.noreply.github.com>
2023-06-07 21:25:09 -0400

    Update arp_poisoning_nids.py

--- Cycle 3 - ARPPoisoningNIDS/arp_poisoning_nids.py
+++ Cycle 3 - ARPPoisoningNIDS/arp_poisoning_nids.py
@@ -107,7 +107,7 @@ def get_ipAddress_reservations():
                     if isinstance(get_tbl_timestamp_diff, str):
                         pass
                     else:
-                        if ((mac_address == check_tbl_entry) and (float(timestamp_diff_int)) >= get_tbl_timestamp_diff)): # 
+                        if ((mac_address == check_tbl_entry) and (float(timestamp_diff_int) >= get_tbl_timestamp_diff)): # 
                             update_query = """UPDATE ipam_db_reservations SET tbl_host_name = ?, tbl_reserved_ip = ?, tbl_mac_address = ?, tbl_lease_time = ?, tbl_lease_expire = ?, tbl_time_stamp = ?, tbl_timestamp_diff = ? WHERE tbl_reserved_ip = ? AND tbl_mac_address = ?"""
                             update_values = (host_name, reserved_ip, mac_address, lease_time, lease_expire, time_stamp, timestamp_diff, reserved_ip, mac_address)
                             cur.execute(update_query, update_values)
