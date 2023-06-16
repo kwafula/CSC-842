fb77aecf29fccfd2c52be45fcbeeb2cc34cb59be
Kevin Wafula <95890992+kwafula@users.noreply.github.com>
2023-06-16 15:50:38 -0400

    Update burnerlockbox.py

--- Cycle 5/burnerlockbox.py
+++ Cycle 5/burnerlockbox.py
@@ -1,1 +1,11 @@
+#!/usr/bin/env python3 
 
+import scapy.all as scapy
+import csv
+import json
+import argparse
+import sqlite3
+import requests
+from requests.structures import CaseInsensitiveDict
+import time
+from datetime import datetime
