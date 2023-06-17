5e1f640d64c540f67c81ab7b14f650b642614ea1
Kevin Wafula <95890992+kwafula@users.noreply.github.com>
2023-06-17 05:28:18 -0400

    Update burner_lockbox.py

--- Cycle5_Burner-Lockbox/burner_lockbox.py
+++ Cycle5_Burner-Lockbox/burner_lockbox.py
@@ -16,9 +16,9 @@ def parseArguments():
     
     subparser = parser.add_subparsers(dest='command')
     
-    mkdir = subparser.add_parser('mkdir', description='subparser description', help='Command to make a temporary directory:\n'
-                                 'Required arguements: --dir <directory name>\n'
-                                 'Example: python3 burner_lockbox.py mkdir --dir /opt/tempveracrypt\n\n')
+    mkdir = subparser.add_parser('mkdir', help='Command to make a temporary directory,\n'
+                                 'Required arguements: --dir <directory name>,\n'
+                                 'Usage: python3 burner_lockbox.py mkdir --dir /opt/tempveracrypt\n\n')
     create_container = subparser.add_parser('create_container')
     mount_container = subparser.add_parser('mount_container')
     cp_file = subparser.add_parser('cp_file')
