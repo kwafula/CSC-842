a534ee4cd6a4adef83e4d6ead761fd025bb0a9ee
Kevin Wafula <95890992+kwafula@users.noreply.github.com>
2023-06-18 08:53:50 -0400

    Update burner_lockbox.py

--- Cycle5_Burner-Lockbox/burner_lockbox.py
+++ Cycle5_Burner-Lockbox/burner_lockbox.py
@@ -228,7 +228,7 @@ def run_shell_command(shell_cmd):
             print("exception occurred", ex)
             return f"   [subprocess broke]"
     else:
-        print('[+] Done!')
+        break
      
 def run_pkg_check(shell_cmd):
     try:
