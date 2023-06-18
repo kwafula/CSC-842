7086286c6645450a5a1b01fbc02f5e72cfbdc1cc
Kevin Wafula <95890992+kwafula@users.noreply.github.com>
2023-06-18 02:38:53 -0400

    Update burner_lockbox.py

--- Cycle5_Burner-Lockbox/burner_lockbox.py
+++ Cycle5_Burner-Lockbox/burner_lockbox.py
@@ -113,37 +113,37 @@ def parseArguments():
       
     elif args.function == 'create_lockbox':
         cmd_string = 'veracrypt --text --create ' + args.name + ' --size ' + args.size + ' --password ' + args.password + ' --volume-type ' + args.type + ' --encryption AES --hash sha-512 --filesystem exfat --pim 0 --keyfiles "" --random-source /dev/urandom'
-        print('[+] This option will create the following lockbox: ', args.name))
+        print('[+] This option will create the following lockbox: ', args.name)
         print('')
         # veracrypt --text --create vctest.vc --size 200M --password MySuperSecurePassword1! --volume-type normal --encryption AES --hash sha-512 --filesystem exfat --pim 0 --keyfiles "" --random-source /dev/urandom 
         
     elif args.function == 'mount_lockbox':
         cmd_string = 'veracrypt --text --mount ' + args.name + ' --password ' + args.password + ' --pim 0 --keyfiles "" --protect-hidden no'  
-        print('[+] This option will mount the following lockbox: ', args.name))
+        print('[+] This option will mount the following lockbox: ', args.name)
         print('')
         # veracrypt --text --mount vctest.vc /mnt --password MySuperSecurePassword1! --pim 0 --keyfiles "" --protect-hidden no --slot 1 --verbose
         
     elif args.function == 'list_lockbox':
         cmd_string = 'veracrypt --text --list'
-        print('[+] This option will list the following lockbox: ', args.name))
+        print('[+] This option will list the following lockbox: ', args.name)
         print('')
         # veracrypt --text --list
         
     elif args.function == 'dismount_lockbox':
         cmd_string = 'veracrypt --text --dismount ' + args.name
-        print('[+] This option will dismount the following lockbox: ', args.name))
+        print('[+] This option will dismount the following lockbox: ', args.name)
         print('')
         # veracrypt --text --dismount vctest.vc
         
     elif args.function == 'upload_lockbox':
         cmd_string = 'curl -T ' + args.name + ' ' + args.url
-        print('[+] This option will upload the following lockbox: ', args.name))
+        print('[+] This option will upload the following lockbox: ', args.name)
         print('')
         # curl -T monocacygatewayplan800w.jpg https://filebin.net/p5oig73mhgaieu04/
         
     elif args.function == 'download_lockbox':
         cmd_string = 'curl -L ' + args.url + ' --output ' + args.name
-        print('[+] This option will download the following lockbox: ', args.name))
+        print('[+] This option will download the following lockbox: ', args.name)
         print('')
         # curl -L https://filebin.net/p5oig73mhgaieu04//monocacygatewayplan800w.jpg --output monocacygatewayplan800w-2.jpg
     
