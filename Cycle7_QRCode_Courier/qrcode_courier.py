3003fc2d9135f0a83aa2480175a13137a57c63bc
Kevin Wafula <95890992+kwafula@users.noreply.github.com>
2023-07-01 05:08:09 -0400

    Update qrcode_courier.py

--- Cycle7_QRCode_Courier/qrcode_courier.py
+++ Cycle7_QRCode_Courier/qrcode_courier.py
@@ -113,7 +113,8 @@ if args.command == 'encode':
 
     # Set size of QR code
     #pos = ((icon.size[0] - icon_logo.size[0]) // 2, (icon.size[1] - icon_logo.size[1]) // 2)
-    icon.paste(icon_logo, pos)
+    #icon.paste(icon_logo, pos)
+    icon.paste(icon_logo)
 
     # Save icon
     output_file = args.output_file
