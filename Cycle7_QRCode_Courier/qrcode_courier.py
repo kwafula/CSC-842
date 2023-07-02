777afcca9ad192bdb959a1ffaaaf87bfcd36ae56
Kevin Wafula <95890992+kwafula@users.noreply.github.com>
2023-07-02 06:07:24 -0400

    Update qrcode_courier.py

--- Cycle7_QRCode_Courier/qrcode_courier.py
+++ Cycle7_QRCode_Courier/qrcode_courier.py
@@ -114,7 +114,7 @@ if args.command == 'encode':
 
     # Encode data into QR Code
     #icon = qr_percel.make_image(back_color=(255, 195, 235), fill_color=(55, 95, 35))
-    icon = qr_percel.make_image(back_color=(36, 31, 49), fill_color=(0, 0, 0)) # (21, 18, 29), fill_color=(0, 0, 0))
+    icon = qr_percel.make_image(back_color=(255, 255, 255), fill_color=(0, 0, 0)) # (21, 18, 29), fill_color=(0, 0, 0)) , (36, 31, 49), fill_color=(0, 0, 0))
     print("[+] Creating QR Code..............................................")
     print("")
 
@@ -204,8 +204,8 @@ elif args.command == 'decode':
     print(qr_image)
     data, vertices_array, binary_qrcode = detector.detectAndDecode(qr_image)
     print("")
-    print(data)
-    print(vertices_array)
+    print("Data:\n ", data)
+    print("Vertices_Array:\n ", vertices_array)
 
     # If there is a QR code, decode and print the data
     output_file = args.output_file
