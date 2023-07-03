3565f3b2f31295cb303796b435f2bc2383266344
Kevin Wafula <95890992+kwafula@users.noreply.github.com>
2023-07-03 06:27:28 -0400

    Update qrcode_courier.py

--- Cycle7_QRCode_Courier/qrcode_courier.py
+++ Cycle7_QRCode_Courier/qrcode_courier.py
@@ -85,8 +85,8 @@ if args.command == 'encode':
     # print("")
 
     # Encode data into QR Code
-    #icon = qr_percel.make_image(back_color = (255, 195, 235), fill_color=(55, 95, 35))
-    icon = qr_percel.make_image(back_color = (255, 255, 255), fill_color=(0, 0, 0)) # (21, 18, 29), fill_color=(0, 0, 0)) , (36, 31, 49), fill_color=(0, 0, 0)), (255, 255, 255), fill_color=(0, 0, 0))
+    #icon = qr_percel.make_image(back_color = (255, 195, 235), fill_color=(55, 95, 35))  # (21, 18, 29), fill_color=(0, 0, 0)) , (36, 31, 49), fill_color=(0, 0, 0)), (255, 255, 255), fill_color=(0, 0, 0))
+    icon = qr_percel.make_image(back_color = (255, 255, 255), fill_color=(0, 0, 0))
     print("[+] Creating QR Code..............................................")
     print("")
 
