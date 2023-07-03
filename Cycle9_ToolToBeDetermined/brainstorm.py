ffe0f26570a8ee2ca6dca5315febb1c23ade687c
Kevin Wafula <95890992+kwafula@users.noreply.github.com>
2023-07-03 16:18:43 -0400

    Update brainstorm.py

--- Cycle9_ToolToBeDetermined/brainstorm.py
+++ Cycle9_ToolToBeDetermined/brainstorm.py
@@ -1,1 +1,4 @@
-
+FACTS
+QR Codes file forms come in JPEG,PNG, SVG, EPS, and PDF,  and so are the file magic signatures
+  https://www.qr-code-generator.com/guides/download-qr-codes-into-different-file-formats/#:~:text=QR%20Codes%20can%20then%20be,with%20QR%20Code%20Generator%20software.
+QR Codes have 
