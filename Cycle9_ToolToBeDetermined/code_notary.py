a624d410e0db878fe5c2f8249fe3e43b9f5827c2
Kevin Wafula <95890992+kwafula@users.noreply.github.com>
2023-07-08 20:45:55 -0400

    Create code_notary.py

---
+++ Cycle9_ToolToBeDetermined/code_notary.py
@@ -0,0 +1,18 @@
+Example uses Letsencrypt
+Prerequisite
+1. Set Letsencrypt environment
+
+Objectives
+1. Code provenence 
+  - Digital signature
+2. Code integrity
+  - Hash code
+3. Code confidentiality
+  - Encryption/ One-way obfusication
+
+Product Backlog
+1. Install dependencies
+2. Display help menu
+3. Create public-key and private-key pair
+4. Create hash of the code
+5. Decrypt and verify code
