b0d7d1af067cf678af86eda670c260e47d89436c
Kevin Wafula <95890992+kwafula@users.noreply.github.com>
2023-07-08 21:00:03 -0400

    Update code_notary.py

--- Cycle9_ToolToBeDetermined/code_notary.py
+++ Cycle9_ToolToBeDetermined/code_notary.py
@@ -1,18 +1,23 @@
-Example uses Letsencrypt
-Prerequisite
-1. Set Letsencrypt environment
+# Example uses Letsencrypt
+# # # Prerequisite
+# 1. Set Letsencrypt environment
 
-Objectives
-1. Code provenence 
-  - Digital signature
-2. Code integrity
-  - Hash code
-3. Code confidentiality
-  - Encryption/ One-way obfusication
+# # # # Objectives
+# 1. Code provenance 
+#   - Digital signature
+# 2. Code integrity
+#  - Hash code
+# 3. Code confidentiality
+#   - Encryption/ One-way obfuscation
 
-Product Backlog
-1. Install dependencies
-2. Display help menu
-3. Create public-key and private-key pair
-4. Create hash of the code
-5. Decrypt and verify code
+# # # Product Backlog
+# 1. Install dependencies
+# 2. Display help menu
+# 3. Create public-key and private-key pair
+# 4. Create hash of the code
+# 5. Decrypt and verify code
+# 6. HTTP ACME validation
+# 7. DNS ACME validation
+# 8. TLS ACME validaton
+
+
