b3311ead1daa8274bfd258ffb6e18ff1000317a1
Kevin Wafula <95890992+kwafula@users.noreply.github.com>
2023-07-17 08:24:13 -0400

    Update qrcode_inspector.py

--- Cycle9_QRCode_Inspector/qrcode_inspector.py
+++ Cycle9_QRCode_Inspector/qrcode_inspector.py
@@ -8,60 +8,44 @@ import os
 import base64
 import requests
 import json
-import subprocess
 import urllib.parse
-
-def fetch_virus_total_record(hostname: str, api_key: str) -> dict[str, any]:
-  
-  headers = {"x-apikey": api_key}
-  encoded_hostname = base64.b64encode(hostname.encode("utf8")).decode("utf8").replace("=", "")
-  url = f"https://www.virustotal.com/api/v3/urls/{encoded_hostname}"
-  response = requests.get(url, headers=headers)
-
-  return response.json()
-
-        
-def run_shell_command(shell_cmd):
-    if shell_cmd is not None: 
-        try:
-            pro = subprocess.run(shell_cmd, capture_output=True, text=True, shell=True)
-            if pro.stdout:
-                return f"---------------Open Services---------------\n {pro.stdout}", pro.stdout
-            elif pro.stderr:
-                return f"---------------STDERR Detail---------------\n {pro.stderr}"
-        except Exception as ex:
-            print("exception occurred", ex)
-            return f"   [subprocess broke]"
 
 parser = argparse.ArgumentParser(formatter_class = argparse.RawTextHelpFormatter, description = 'QRCode Inspector Usage Details:',\
                                  usage = '       python3 qrcode_inspector.py localFile -f|--file <local_file_path>,\n\n')
+
 subparser = parser.add_subparsers(dest = 'command')
+
 localFile = subparser.add_parser('localFile', formatter_class = argparse.RawTextHelpFormatter, help = 'Description: Inspect local QR Code image file,\n'
                     'Usage Example: python3 qrcode_inspector.py localFile -f ./image1.jpg \n'
                     '               python3 qrcode_inspector.py localFile --file ./image1.jpg \n\n')
-localFile.add_argument('-f', '--file', action = 'store', type=str, dest = 'input_file', required = True)
-parser.add_argument('-v', '--version', action='version', version='%(prog)s v1.0')
-args = parser.parse_args()
 
+localFile.add_argument('-f', '--file', action = 'store', type=str, dest = 'input_file', required = True)
+
+parser.add_argument('-v', '--version', action='version', version='%(prog)s v1.0')
+
+args = parser.parse_args()
 
 qrcode_local_file = None
 qrcode_file_obj = None
 qrcode_decoded_url = None
 qrcode_decoded_hostname = None
-virus_total_apikey = input("Enter the Virus Total API Key :")
+virus_total_apikey = input("Enter the Virus Total API Key : ")
+print("")
 
 if args.command == 'localFile':
     # Troubleshooting code
-    print(os.path.exists(args.input_file))
-    print("")
+    # print(os.path.exists(args.input_file))
+    # print("")
 
     qrcode_local_file = args.input_file
     
     if args.input_file and os.path.exists(args.input_file):
         print(f"[+] Reading the following QR Code file:\n {os.path.basename(args.input_file)}")
         qrcode_file_obj = cv2.imread(qrcode_local_file)
-        print("QR Code File Object:\n ", qrcode_file_obj)
-        print("")
+
+        # Troubleshooting code
+        # print("QR Code File Object:\n ", qrcode_file_obj)
+        # print("")
     
     # Initialize the cv2 QRCode detector
     print("[+] Initializing decoder........................")
@@ -69,7 +53,7 @@ if args.command == 'localFile':
     print("")
           
     # Detect and decode
-    print("[+] Extracting content............................")
+    print("[+] Decoding content............................")
     data, vertices_array, binary_qrcode = detector.detectAndDecode(qrcode_file_obj)
     print("")
     
@@ -87,18 +71,27 @@ if args.command == 'localFile':
   
     # Get hostname
     qrcode_decoded_hostname = urllib.parse.urlparse(qrcode_decoded_url)
- 
-    print(qrcode_decoded_hostname)
-    print("The hostname is :", qrcode_decoded_hostname.netloc)
+  
+    # Troubleshooting code
+    # print(qrcode_decoded_hostname)
+    # print("The hostname is :", qrcode_decoded_hostname.netloc)
+    # print("")
     
     headers = {"x-apikey": virus_total_apikey}
     encoded_hostname = base64.b64encode(qrcode_decoded_hostname.netloc.encode("utf8")).decode("utf8").replace("=", "")
     virus_total_url = f"https://www.virustotal.com/api/v3/urls/{encoded_hostname}"
     virus_total_response = requests.get(virus_total_url, headers=headers)
     json_data = virus_total_response.json()
-    json_object = json.loads(json_data)
-    json_formatted_str = json.dumps(json_object, indent=2)
-    print(json_formatted_str)
+    virus_total_report_data = json.dumps(json_data, sort_keys=True, indent=4)
+    print(virus_total_report_data)
+    print("")
+
+    base = os.path.basename('/root/dir/sub/file.ext')
+    virus_total_report_file = os.path.splitext(base)[0]
+    
+    with open((virus_total_report_file + "json"), mode="w") as file_obj: 
+        json.dump(json_data, file_obj)
+        return 
     
 else:
     print('Error: Verify command arguments and run the program again')
