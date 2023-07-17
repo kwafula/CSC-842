ca258f4a00505852466c94d8f90040ff192c3e24
Kevin Wafula <95890992+kwafula@users.noreply.github.com>
2023-07-17 00:18:22 -0400

    Update qrcode_inspector.py

--- Cycle9_QRCode_Inspector/qrcode_inspector.py
+++ Cycle9_QRCode_Inspector/qrcode_inspector.py
@@ -47,7 +47,7 @@ parser = argparse.ArgumentParser(formatter_class = argparse.RawTextHelpFormatter
                                  '       python3 qrcode_inspector.py readServices,\n'
                                  '       python3 qrcode_inspector.py localFile -f|--file <local_file_path>,\n'
                                  '       python3 qrcode_inspector.py remoteFile -u|--url <remote_file_url>,\n'
-                                 '       python3 qrcode_inspector.py remoteScrape -u|--url <remote_website_url>,\n\n')
+                                 '       python3 qrcode_inspector.py remoteSite -u|--url <remote_website_url>,\n\n')
 subparser = parser.add_subparsers(dest = 'command')
 
 addService = subparser.add_parser('addKey', formatter_class = argparse.RawTextHelpFormatter, help = 'Description: Add CTI service for analysis,\n'
@@ -69,9 +69,9 @@ remoteFile = subparser.add_parser('remoteFile', formatter_class = argparse.RawTe
                     'Usage Example: python3 qrcode_inspector.py remoteFile -u "https://github.com/kwafula/CSC-842/blob/main/Cycle9_QRCode_Inspector/image1.jpg"\n'
                     '               python3 qrcode_inspector.py remoteFile --url "https://github.com/kwafula/CSC-842/blob/main/Cycle9_QRCode_Inspector/image2.png"\n\n')
 
-remoteScrape = subparser.add_parser('remoteScrape', formatter_class = argparse.RawTextHelpFormatter, help = 'Description: Scrape remote website and inspect enumerated QR Code image files,\n'
-                    'Usage Example: python3 qrcode_inspector.py remoteScrape -u "https://kwafula.github.io" \n'
-                    '               python3 qrcode_inspector.py remoteScrape --url "https://kwafula/kwafula.github.io" \n\n')
+remoteSite = subparser.add_parser('remoteSite', formatter_class = argparse.RawTextHelpFormatter, help = 'Description: Scrape remote website and inspect enumerated QR Code image files,\n'
+                    'Usage Example: python3 qrcode_inspector.py remoteSite -u "https://kwafula.github.io" \n'
+                    '               python3 qrcode_inspector.py remoteSite --url "https://kwafula/kwafula.github.io" \n\n')
 
 addService.add_argument('-s', '--service', action = 'store', type=str, dest = 'cti_service_name', required = True)
 addService.add_argument('-a', '--api', action = 'store', type=str, dest = 'cti_service_name', required = True)
@@ -85,13 +85,15 @@ localFile.add_argument('-f', '--file', action = 'store', type=str, dest = 'input
 
 remoteFile.add_argument('-r', '--url', action = 'store', type=str, dest = 'input_url', required = True)
 
-remoteScrape.add_argument('-u', '--url', action = 'store', type=str, dest = 'input_url', required = True)
+remoteSite.add_argument('-u', '--url', action = 'store', type=str, dest = 'input_url', required = True)
 
 parser.add_argument('-v', '--version', action='version', version='%(prog)s v1.0')
 
 args = parser.parse_args()
 
-qrcode_file_path = None
+qrcode_local_file = None
+qrcode_remote_file = None
+qrcode_remote_site = None
 qrcode_file_hash = None
 qrcode_file_obj = None
 qrcode_decoded_url = None
@@ -117,11 +119,10 @@ elif args.command == 'localFile':
     print(os.path.exists(args.input_file))
     print("")
     if args.input_file and os.path.exists(args.input_file):
-        qrcode_file_path = args.input_file
-        print(f"[+] Reading the following file:\n {os.path.basename(qrcode_file_path)}")
-        qrcode_file_obj = cv2.imread(qrcode_file_path)
+        print(f"[+] Reading the following file:\n {os.path.basename(args.input_file)}")
+        qrcode_local_file = args.input_file
+        qrcode_file_obj = cv2.imread(qrcode_local_file)
         print("QR Code File Object:\n ", qrcode_file_obj)
-        print("")
         print("")
     
     # FIlE HASH FUNCTION HERE 
@@ -165,8 +166,17 @@ elif args.command == 'localFile':
         write_file(output_file, source_data)
     '''
 elif args.command == 'remoteFile':
-    print("Remote File feature yet to be implemented")
-elif args.command == 'remoteFile':
+    if args.input_url:
+        qrcode_remote_file = args.input_url
+        qrcode_url_response = requests.get(qrcode_remote_file, allow_redirects=True)
+        if qrcode_url_response.status_code == 200:
+            print(f"[+] Download QR Code image at the following url:\n {qrcode_remote_file}")
+            print("[+] QR Code image download status code:\n ", qrcode_url_response.status_code)
+            print("")
+        else:
+            print("")
+            print("QR Code image file does not exist and the following URL : ", qrcode_remote_file)
+elif args.command == 'remoteSite':
     print("Remote Scrape feature yet to be implemented")
 else:
     print('Error: Verify command arguments and run the program again')
