1d1e106b82c132a18b437ded81f8919aa476c8f8
Kevin Wafula <95890992+kwafula@users.noreply.github.com>
2023-07-17 04:31:55 -0400

    Update qrcode_inspector.py

--- Cycle9_QRCode_Inspector/qrcode_inspector.py
+++ Cycle9_QRCode_Inspector/qrcode_inspector.py
@@ -89,8 +89,8 @@ if args.command == 'localFile':
     sha256_set_cmd = "sha256=$(sha256sum " + qrcode_local_file + ")" 
     qrcode_file_hash = run_shell_command(sha256_set_cmd)
     print(sha256_set_cmd)
-    sha256_echo = "echo $sha256"
-    qrcode_file_hash = run_shell_command(sha256_echo)
+    sha256_echo_cmd = "echo ${sha256/ *}"
+    qrcode_file_hash = run_shell_command(sha256_echo_cmd)
     print(qrcode_file_hash)
     sha256_unset_cmd = "unset sha256"
     qrcode_file_hash = run_shell_command(sha256_unset_cmd)
