e28ba4eb6b91347bc343a1b0d15a919ac501c1b9
Kevin Wafula <95890992+kwafula@users.noreply.github.com>
2023-07-17 06:19:06 -0400

    Update qrcode_inspector.py

--- Cycle9_QRCode_Inspector/qrcode_inspector.py
+++ Cycle9_QRCode_Inspector/qrcode_inspector.py
@@ -3,31 +3,23 @@
 import qrcode
 from PIL import Image
 import cv2
-# import numpy as np
-# import time
 import argparse
 import os
-# import base64
-# import requests
-# import shutil
+import base64
+import requests
+import json
 import subprocess
 
 
-# Configure Virus Total API Key
+def fetch_virus_total_record(hostname: str, api_key: str) -> dict[str, any]:
+  
+  headers = {"x-apikey": api_key}
+  encoded_hostname = base64.b64encode(hostname.encode("utf8")).decode("utf8").replace("=", "")
+  url = f"https://www.virustotal.com/api/v3/urls/{encoded_hostname}"
+  response = requests.get(url, headers=headers)
 
-# Read file from local file system, return file object
-def read_file(file_name):
-    with open(file_name, mode="r", encoding="utf8") as file_obj:
-        file_data = file_obj.read()
-        return file_data
+  return response.json()
 
-# Function to write data file
-# Excluding the encoding="utf8" argument, not sure how it might image QR Code data
-def write_file(file_name, file_data):
-    with open(file_name, mode="wb") as file_obj: 
-        file_obj.write(file_data)
-        file_obj.close()
-        return 
         
 def run_shell_command(shell_cmd):
     if shell_cmd is not None: 
@@ -40,41 +32,23 @@ def run_shell_command(shell_cmd):
         except Exception as ex:
             print("exception occurred", ex)
             return f"   [subprocess broke]"
-        
-# Run hash of image file, return image file hash (QA test if image file hash is the same for manual download and script download) 
-
-# Extract QR Code URL and Domain, return URL and Domain
-
-# Check if image file is QR Code, if QR Code, return QR Code image file object 
-
-# Check if QR Code content has URL content, if yes, return image file hash, URL, domain, and IP Address,
-#     else, display QR Code content for manual review to decide if it should be saved for offline analysis
-
-# Run URL, hash, domain, and IP Address checks in VirusTotal sequentially, returns results sequentially
-
 
 parser = argparse.ArgumentParser(formatter_class = argparse.RawTextHelpFormatter, description = 'QRCode Inspector Usage Details:',\
                                  usage = '       python3 qrcode_inspector.py localFile -f|--file <local_file_path>,\n\n')
 subparser = parser.add_subparsers(dest = 'command')
-
 localFile = subparser.add_parser('localFile', formatter_class = argparse.RawTextHelpFormatter, help = 'Description: Inspect local QR Code image file,\n'
                     'Usage Example: python3 qrcode_inspector.py localFile -f ./image1.jpg \n'
                     '               python3 qrcode_inspector.py localFile --file ./image1.jpg \n\n')
-
 localFile.add_argument('-f', '--file', action = 'store', type=str, dest = 'input_file', required = True)
-
 parser.add_argument('-v', '--version', action='version', version='%(prog)s v1.0')
-
 args = parser.parse_args()
 
+
 qrcode_local_file = None
-qrcode_file_hash = None
 qrcode_file_obj = None
 qrcode_decoded_url = None
-qrcode_decoded_domain = None
-qrcode_decoded_ip = None
-virus_total_api = None
-virus_total_apikey = None
+qrcode_decoded_domain_name = None
+virus_total_apikey = input("Enter the Virus Total API Key :")
 
 if args.command == 'localFile':
     # Troubleshooting code
@@ -82,18 +56,6 @@ if args.command == 'localFile':
     print("")
 
     qrcode_local_file = args.input_file
-    
-    # Get file hash
-    md5_set_cmd = "md5=$(md5sum " + qrcode_local_file + ")" 
-    print("md5_set_cmd :", md5_set_cmd)
-    qrcode_file_hash = run_shell_command(md5_set_cmd)
-    print("qrcode_file_hash :", qrcode_file_hash)
-    md5_split_cmd = "${md5%% *}"
-    print("md5_split_cmd :", md5_split_cmd)
-    qrcode_file_hash = run_shell_command(md5_split_cmd)
-    print("qrcode_file_hash :", qrcode_file_hash)
-    md5_unset_cmd = "unset md5"
-    run_shell_command(md5_unset_cmd)
     
     if args.input_file and os.path.exists(args.input_file):
         print(f"[+] Reading the following QR Code file:\n {os.path.basename(args.input_file)}")
@@ -110,15 +72,42 @@ if args.command == 'localFile':
     print("[+] Extracting content............................")
     data, vertices_array, binary_qrcode = detector.detectAndDecode(qrcode_file_obj)
     print("")
-
+    
+    qrcode_decoded_url = data
+    
     # Troubleshooting code
+    '''
     print("QR Code Vertices Array:\n ", vertices_array)
     print("")
     print("QR Code Binary Data:\n ", binary_qrcode)
     print("")
     print("QR Code Data:\n ", data)
     print("")
+    '''
+    
+    # Split the input string by backslash
+    x = input_string.split("/")
+    # if input URL string contain HTTPs or HTTP
+    if(x[0] == "https:" or x[0] == "http:"):
+        x = x[2].split(".")
+    # if Input URL String only contain www. without any Http or https.
+    else:
+        
+        x = x[0].split(".")
+    # if input URL without www. or subdomain
+    if(len(x) == 2):
+        qrcode_decoded_domain_name = x[0]
+    # if input URL is with www. or subdomain 
+    else:
+        qrcode_decoded_domain_name = x[1]
 
+    print("The Domain Name is ", qrcode_decoded_domain_name)
+    
+    headers = {"x-apikey": virus_total_apikey}
+    encoded_hostname = base64.b64encode(hostnameqrcode_decoded_domain_name.encode("utf8")).decode("utf8").replace("=", "")
+    virus_total_url = f"https://www.virustotal.com/api/v3/urls/{encoded_hostname}"
+    virus_total_response = requests.get(virus_total_url, headers=headers)
+    print(virus_total_response.json())
     
 else:
     print('Error: Verify command arguments and run the program again')
